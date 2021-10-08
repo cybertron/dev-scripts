15c9036b06d902fffd5eba198934dd35748e35b9
Ben Nemec <bnemec@redhat.com>
2021-10-08 10:12:59 -0500

    Add worker config

--- keepalivedv6/update.sh
+++ keepalivedv6/update.sh
@@ -4,9 +4,11 @@ set -eux
 
 pod_data=$(base64 -w 0 pod.yaml)
 config_data=$(base64 -w 0 config.yaml)
+worker_data=$(base64 -w 0 config-worker.yaml)
 mc_file='keepalivedv6-mc.yaml'
 
-sed -e "s/@POD/$pod_data/" mc.tmpl | sed -e "s/@CONFIG/$config_data/" > $mc_file
+sed -e "s/@POD/$pod_data/" mc.tmpl | sed -e "s/@CONFIG/$config_data/" | sed -e "s/@WORKERCONFIG/$worker_data/" > $mc_file
 
 oc delete mc 50-keepalived-v6 || :
+oc delete mc 50-keepalived-v6-worker || :
 oc create -f $mc_file
